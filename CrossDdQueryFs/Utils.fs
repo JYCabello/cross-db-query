12351380bb7105a2614b89b63a1e3f9d621d4d48
JYCabello <j.cabello.ramos@gmail.com>
2021-03-14 08:41:58 +0100

    ParallelTuple

--- CrossDdQueryFs/Utils.fs
+++ CrossDdQueryFs/Utils.fs
@@ -10,19 +10,49 @@ type OptionBinding() =
 
 let option = OptionBinding()
 
-let parallelTuple5 (async1, async2, async3, async4, async5) =
+let parallelTuple2 (async1, async2) =
   async {
     let! child1 = Async.StartChild (async1())
     let! child2 = Async.StartChild (async2())
-    let! child3 = Async.StartChild (async3())
-    let! child4 = Async.StartChild (async4())
-    let! child5 = Async.StartChild (async5())
     let! result1 = child1
     let! result2 = child2
+    return (result1, result2)
+  }
+
+let parallelTuple3 (async1, async2, async3) =
+  async {
+    let! childTuple = parallelTuple2 (async1, async2) |> Async.StartChild
+    let! child3 = Async.StartChild (async3())
+    let! (result1, result2) = childTuple
     let! result3 = child3
+    return (result1, result2, result3)
+  }
+
+let parallelTuple4 (async1, async2, async3, async4) =
+  async {
+    let! childTuple = parallelTuple3 (async1, async2, async3) |> Async.StartChild
+    let! child4 = Async.StartChild (async4())
+    let! (result1, result2, result3) = childTuple
     let! result4 = child4
+    return (result1, result2, result3, result4)
+  }
+
+let parallelTuple5 (async1, async2, async3, async4, async5) =
+  async {
+    let! childTuple = parallelTuple4 (async1, async2, async3, async4) |> Async.StartChild
+    let! child5 = Async.StartChild (async5())
+    let! (result1, result2, result3, result4) = childTuple
     let! result5 = child5
     return (result1, result2, result3, result4, result5)
+  }
+
+let parallelTuple6 (async1, async2, async3, async4, async5, async6) =
+  async {
+    let! childTuple = parallelTuple5 (async1, async2, async3, async4, async5) |> Async.StartChild
+    let! child6 = Async.StartChild (async6())
+    let! (result1, result2, result3, result4, result5) = childTuple
+    let! result6 = child6
+    return (result1, result2, result3, result4, result5, result6)
   }
 
 let toMap (l: ('a * 'b) list) =
